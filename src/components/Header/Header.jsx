64f0fbe4e02d1179c6541a75f36087e563d310f4
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-25 01:29:44 +0430

    change styles and link singup/login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,21 @@
+import { Link } from 'react-router-dom';
+
 import { HiOutlineLogin } from 'react-icons/hi';
 
 const Header = () => {
 	return (
-		<header className='flex w-full items-center justify-between'>
-			<section>
-				<p>Mhp Shop</p>
-			</section>
+		<header className='flex h-11 w-full items-center justify-between bg-sky-500 px-3'>
+			<Link to='/' className='text-lg font-bold text-white'>
+				<p>MHP Shop</p>
+			</Link>
 
 			<section>
-				<span className='flex items-center justify-center'>
-					<HiOutlineLogin />
-					<p>Login / </p>
-					<p>Signup</p>
-				</span>
+				<Link
+					to='/login'
+					className='flex items-center justify-center rounded-md border-1 px-2 py-1 text-sm'>
+					<HiOutlineLogin className='mr-1 h-4 w-4' />
+					<p>Login / Signup</p>
+				</Link>
 			</section>
 		</header>
 	);
