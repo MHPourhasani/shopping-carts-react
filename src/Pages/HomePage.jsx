4615ddf345afd133c9edf7f45da102bf041d72e4
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-25 01:05:44 +0430

    create Home page components

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Layout from '../Layout/Layout';
+
+const HomePage = () => {
+	return (
+		<main>
+			<Layout>
+                <p>Home Page</p>
+            </Layout>
+		</main>
+	);
+};
+
+export default HomePage;
