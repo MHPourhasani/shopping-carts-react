4c5f30c1b2284f1b3ad618919992f005caa5312d
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-25 01:15:53 +0430

    change HomePage path

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import HomePage from './Pages/HomePage';
 
-const routes = [{ path: './Pages/HomePage.jsx', exact: true, component: HomePage }];
+const routes = [{ path: '/', exact: true, component: HomePage }];
 
 export default routes;
