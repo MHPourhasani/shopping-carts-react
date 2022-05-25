2822c5d581e72d3dec8879764888c4ef7a3fb414
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-25 17:38:46 +0430

    change Product.jsx component name to ProductCardItem.jsx To show prodoucts as a list

--- src/components/Products/ProductCardItem.jsx
+++ src/components/Products/ProductCardItem.jsx
@@ -1,22 +1,30 @@
+import { useCardActions } from '../../Providers/CardProvider';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
-const Products = ({ product }) => {
+const ProductCardItem = ({ product }) => {
+	const dispatch = useCardActions();
 	const { name, price, image, offPrice } = product;
+
+	const addProductHandler = (product) => {
+		dispatch({ type: 'ADD_TO_CARD', payload: product });
+	};
 
 	return (
 		<section className='m-1 flex w-5/12 flex-col items-start justify-between rounded-md border-1 hover:border-sky-500 hover:drop-shadow-xl sm:m-2 sm:w-3/12 lg:w-2/12'>
 			<Link to='/'>
-				<img src={image} alt={name} className=' h-auto w-full rounded-md' />
+				<img src={image} alt={name} loading='lazy' className=' h-auto w-full rounded-md' />
 			</Link>
 
 			<section className='relative w-full bg-gray-100 p-1.5 md:px-2.5'>
-				<button className='absolute -top-4 right-2 flex h-7 w-7 items-center justify-center rounded-md bg-sky-400 text-lg text-white hover:bg-sky-500'>
+				<button
+					onClick={() => addProductHandler(product)}
+					className='absolute -top-4 right-2 flex h-7 w-7 items-center justify-center rounded-md bg-sky-400 text-lg text-white hover:bg-sky-500'>
 					<MdAddShoppingCart />
 				</button>
 
 				<section className='my-1 flex flex-col items-start justify-center'>
-					<Link>
+					<Link to='/'>
 						<p className='mb-1 font-semibold'>{name}</p>
 					</Link>
 
@@ -30,4 +38,4 @@ const Products = ({ product }) => {
 	);
 };
 
-export default Products;
+export default ProductCardItem;
