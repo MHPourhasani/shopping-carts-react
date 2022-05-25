3c424c79cb22f909ea78ffa01ad1c83b93756878
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-26 01:34:25 +0430

    add CardsPage component to routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
+import CardsPage from './Pages/CardsPage';
 import HomePage from './Pages/HomePage';
 
-const routes = [{ path: '/', exact: true, component: HomePage }];
+const routes = [
+	{ path: '/cards', component: CardsPage },
+	{ path: '/', exact: true, component: HomePage },
+];
 
 export default routes;
