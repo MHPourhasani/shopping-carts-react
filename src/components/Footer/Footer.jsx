41f86aefd1c4693ddce72362a216f13e95c4d4f2
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-28 19:48:36 +0430

    show number of products in card in Cards nav link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
+
+import { useCard } from '../../Providers/CardProvider';
 
 import {
 	AiOutlineHome,
@@ -12,6 +14,7 @@ import {
 
 const Footer = () => {
 	const [activeNav, setActiveNav] = useState('home');
+	const { card } = useCard();
 
 	return (
 		<footer className='fixed bottom-0 right-0 flex h-11 w-full items-center justify-around rounded-t-lg bg-sky-500 sm:h-14'>
@@ -29,13 +32,16 @@ const Footer = () => {
 
 			<NavLink
 				to='/cards'
-				onClick={() => setActiveNav('cards')}
-				className={`flex items-center justify-center ${
-					activeNav === 'cards'
+				onClick={() => setActiveNav('cardsPage')}
+				className={`relative flex items-center justify-center ${
+					activeNav === 'cardsPage'
 						? 'rounded-md bg-sky-900 px-2 py-0.5 text-white sm:py-1.5 sm:px-3'
 						: 'opacity-60'
 				}`}>
-				{activeNav === 'cards' ? <AiTwotoneShopping /> : <AiOutlineShopping />}
+				<p className='absolute -top-1 -right-4 flex h-4 w-4 items-center justify-center rounded-full bg-sky-700 text-xs '>
+					{card.length}
+				</p>
+				{activeNav === 'cardsPage' ? <AiTwotoneShopping /> : <AiOutlineShopping />}
 				<p className='ml-1.5 text-sm'>Cards</p>
 			</NavLink>
 
@@ -54,4 +60,4 @@ const Footer = () => {
 	);
 };
 
-export default Footer;
+export default withRouter(Footer);
