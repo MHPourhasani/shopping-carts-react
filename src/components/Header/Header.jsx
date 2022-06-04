dd1088d88593cc7f6b70a0c72e70a2b97e527d10
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-06-04 16:18:49 +0430

    change login icon and change some styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,19 @@
 import { Link } from 'react-router-dom';
 
-import { HiOutlineLogin } from 'react-icons/hi';
+import { FiUser } from 'react-icons/fi';
 
 const Header = () => {
 	return (
-		<header className='flex h-11 w-full items-center justify-between bg-sky-500 px-3 sm:h-14 lg:justify-around lg:text-lg'>
-			<Link to='/' className='text-lg font-bold text-white'>
+		<header className='flex h-11 w-full items-center justify-between bg-sky-500 px-5 text-white sm:h-14 lg:justify-around lg:text-lg'>
+			<Link to='/' className='text-lg font-bold'>
 				<p>MHP Shop</p>
 			</Link>
 
-			<section>
-				<Link
-					to='/login'
-					className='flex items-center justify-center rounded-md border-1 px-2 py-1 text-sm'>
-					<HiOutlineLogin className='mr-1 h-4 w-4' />
-					<p>Login / Signup</p>
-				</Link>
-			</section>
+			<Link
+				to='/login'
+				className='flex items-center justify-center rounded-full p-1 text-2xl hover:bg-gray-100 hover:text-black'>
+				<FiUser />
+			</Link>
 		</header>
 	);
 };
