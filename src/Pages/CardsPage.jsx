e4ca4f40a6aa09204240d0887b8a096d677f882f
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-06-06 02:21:16 +0430

    change some styles and create Card Summary to calculate the products price

--- src/Pages/CardsPage.jsx
+++ src/Pages/CardsPage.jsx
@@ -8,18 +8,10 @@ import empty_cart from '../asset/image/empty_cart.png';
 import { BsTrash } from 'react-icons/bs';
 
 const CardsPage = () => {
-	const { card } = useCard();
+	const { card, total } = useCard();
 	const dispatch = useCardActions();
 
-	const addProductHandler = (product) => {
-		dispatch({ type: 'ADD_TO_CARD', payload: product });
-	};
-
-	const decProductHandler = (product) => {
-		dispatch({ type: 'DECREASE_FROM_CARD', payload: product });
-	};
-
-	if (!card) {
+	if (!card.length) {
 		return (
 			<Layout>
 				<main className='mt-16 flex flex-col items-center justify-center'>
@@ -39,10 +31,20 @@ const CardsPage = () => {
 		);
 	}
 
+	// increase products handler function
+	const addProductHandler = (product) => {
+		dispatch({ type: 'ADD_TO_CARD', payload: product });
+	};
+
+	// decrease products handler function
+	const decProductHandler = (product) => {
+		dispatch({ type: 'DECREASE_FROM_CARD', payload: product });
+	};
+
 	return (
 		<Layout>
-			<main className='mt-20 flex w-full flex-col items-center justify-center md:flex-row md:items-start'>
-				<section className='my-1 w-11/12 rounded-md border-2 border-sky-500 p-3 drop-shadow-md md:mr-2 md:w-7/12 lg:w-5/12'>
+			<main className='mt-20 flex w-full flex-col items-center justify-center sm:flex-row sm:items-start'>
+				<section className='my-1 w-11/12 rounded-md border-2 border-sky-500 p-3 drop-shadow-md sm:mr-2 sm:w-7/12 lg:w-5/12 xl:max-w-md'>
 					<p className='text-sm font-bold'>Your Products</p>
 
 					{card.length
@@ -50,12 +52,12 @@ const CardsPage = () => {
 								<section
 									key={item.id}
 									className='my-2 flex items-center justify-between border-b-1 pb-4'>
-									<Link to='/'>
+									<Link to='/' className='hover:opacity-75'>
 										<img
-											src={item.image}
+											src={item.images[0]}
 											alt={item.name}
 											loading='lazy'
-											className=' h-20 w-20 rounded-md'
+											className='h-20 w-20 rounded-md'
 										/>
 									</Link>
 
@@ -66,23 +68,28 @@ const CardsPage = () => {
 
 										<div className='flex items-center'>
 											<p className='mr-2 text-sm text-gray-400 line-through'>
-												$ {item.price}
+												$ {item.price * item.quantity}
 											</p>
-											<p className='text-sm'>$ {item.offPrice}</p>
+											<p className='text-sm'>
+												$ {item.offPrice * item.quantity}
+											</p>
 										</div>
 									</section>
 
 									<section className='flex items-center justify-center'>
+										{/* increase product btn */}
 										<button
 											onClick={() => decProductHandler(item)}
 											className='mr-1 flex h-5 w-5 items-center justify-center rounded-md bg-red-50 font-semibold text-red-600'>
 											{item.quantity === 1 ? <BsTrash /> : '-'}
 										</button>
 
+										{/* products quantity btn */}
 										<button className='mx-1 flex h-5 w-5 items-center justify-center rounded-md bg-gray-100'>
 											{item.quantity}
 										</button>
 
+										{/* decrease products btn */}
 										<button
 											onClick={() => addProductHandler(item)}
 											className='ml-1 flex h-5 w-5 items-center justify-center rounded-md bg-green-100 font-semibold text-green-600'>
@@ -93,9 +100,50 @@ const CardsPage = () => {
 						  ))
 						: ''}
 				</section>
+
+				<CardSummary />
 			</main>
 		</Layout>
 	);
 };
 
 export default CardsPage;
+
+const CardSummary = () => {
+	const { card, total } = useCard();
+
+	const originalTotalPrice = card.length
+		? card.reduce((acc, curr) => acc + curr.quantity * curr.price, 0)
+		: 0;
+
+	return (
+		<section className='mt-1 mb-16 w-11/12 rounded-md border-2 border-sky-500 p-3 drop-shadow-md sm:w-4/12 md:ml-2 lg:w-3/12'>
+			<p className='text-sm font-bold'>Card Summary</p>
+
+			<section>
+				<div className='my-2 flex items-center justify-between text-sm'>
+					<p>Total Price</p>
+					<p className='font-semibold'>$ {originalTotalPrice}</p>
+				</div>
+
+				<div className='my-2 flex items-center justify-between border-b-1 border-gray-400 pb-3 text-xs'>
+					<p>Off Price</p>
+					<p className='font-semibold'>$ {originalTotalPrice - total}</p>
+				</div>
+
+				<div className='my-2 flex items-center justify-between text-sm font-semibold'>
+					<p className=''>Net Price</p>
+					<p className=''>$ {total}</p>
+				</div>
+			</section>
+
+			<Link to='/checkout'>
+				<button
+					type='submit'
+					className='mt-2 w-full rounded-md bg-sky-400 py-1.5 text-sm font-semibold text-white'>
+					Checkout
+				</button>
+			</Link>
+		</section>
+	);
+};
