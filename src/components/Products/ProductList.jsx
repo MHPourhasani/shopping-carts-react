98cab071f8e2a9c754ff5bc31b49a65527b9e326
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-06-06 01:50:35 +0430

    change some styles to responsive for all device

--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -10,27 +10,23 @@ const ProductList = () => {
 	const [typeShowProducts, setTypeShowProducts] = useState('card');
 
 	return (
-		<section className='mt-16 w-full flex flex-col items-center justify-center'>
-			<section className='my-1 flex w-10/12 items-center justify-between md:w-7/12 lg:w-1/2'>
-				<p className='text-lg font-semibold'>Products</p>
+		<section className='mt-14 flex w-full flex-col items-center justify-center sm:mt-16'>
+			<section className='my-1 flex w-11/12 items-center justify-between sm:w-8/12 md:w-6/12 lg:w-1/2'>
+				<p className='text-lg font-semibold lg:font-bold'>Products</p>
 
 				<div className='flex items-center justify-center text-lg'>
 					<button
 						onClick={() => setTypeShowProducts('list')}
-						className={`mx-1 w-6 h-6 flex justify-center items-center ${
-							typeShowProducts === 'list'
-								? 'rounded-md bg-sky-500 text-white'
-								: ''
+						className={`mr-1 flex h-6 w-6 items-center justify-center rounded-md hover:bg-sky-300 md:h-8 md:w-8 lg:h-8 lg:w-8 ${
+							typeShowProducts === 'list' ? 'bg-sky-500 text-white' : ''
 						}`}>
 						<AiOutlineUnorderedList />
 					</button>
 
 					<button
 						onClick={() => setTypeShowProducts('card')}
-						className={`mx-1 w-6 h-6 flex justify-center items-center ${
-							typeShowProducts === 'card'
-								? 'rounded-md bg-sky-500 text-white'
-								: ''
+						className={`ml-1 flex h-6 w-6 items-center justify-center rounded-md hover:bg-sky-300 md:h-8 md:w-8 lg:h-8 lg:w-8 ${
+							typeShowProducts === 'card' ? 'bg-sky-500 text-white' : ''
 						}`}>
 						<BsGrid />
 					</button>
@@ -39,7 +35,7 @@ const ProductList = () => {
 
 			<section className='flex w-full items-center justify-center'>
 				{typeShowProducts === 'card' ? (
-					<section className='flex w-full flex-wrap items-start justify-center'>
+					<section className='flex w-full flex-wrap items-stretch justify-center'>
 						{data.products.map((product) => {
 							return (
 								<ProductCardItem
