2fb13f07d0df762746150923a140e670c409b190
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-06-06 01:38:58 +0430

    change product image and change some styles

--- src/components/Products/ProductCardItem.jsx
+++ src/components/Products/ProductCardItem.jsx
@@ -1,5 +1,5 @@
 import { useCardActions, useCard } from '../../Providers/CardProvider';
-import { MdAddShoppingCart,MdShoppingCart } from 'react-icons/md';
+import { MdAddShoppingCart, MdShoppingCart } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
 import { checkInCard } from '../../utils/CheckInCard';
@@ -7,22 +7,29 @@ import { checkInCard } from '../../utils/CheckInCard';
 const ProductCardItem = ({ product }) => {
 	const { card } = useCard();
 	const dispatch = useCardActions();
-	const { name, price, image, offPrice } = product;
+	const { name, price, images, offPrice } = product;
 
+	// add products handler function
 	const addProductHandler = (product) => {
 		dispatch({ type: 'ADD_TO_CARD', payload: product });
 	};
 
 	return (
-		<section className='m-1 flex w-5/12 flex-col items-start justify-between rounded-md border-1 hover:border-sky-500 hover:drop-shadow-xl sm:m-2 sm:w-3/12 lg:w-2/12'>
-			<Link to='/'>
-				<img src={image} alt={name} loading='lazy' className=' h-auto w-full rounded-md' />
+		<section className='m-1 flex w-5/12 flex-col items-start justify-between rounded-md border-1 hover:border-sky-500 hover:drop-shadow-xl sm:m-2 sm:w-3/12 md:w-1/5 lg:w-2/12'>
+			<Link to='/' className='hover:opacity-75'>
+				<img
+					src={images[0]}
+					alt={name}
+					loading='lazy'
+					className='h-auto w-full rounded-md'
+				/>
 			</Link>
 
-			<section className='relative w-full bg-gray-100 p-1.5 md:px-2.5'>
+			<section className='relative w-full p-1.5 md:px-2.5'>
+				{/* add products btn */}
 				<button
 					onClick={() => addProductHandler(product)}
-					className='absolute -top-4 right-2 flex h-7 w-7 items-center justify-center rounded-md bg-sky-400 text-lg text-white hover:bg-sky-500'>
+					className='absolute -top-4 right-2 flex h-8 w-8 items-center justify-center rounded-md bg-sky-400 text-lg text-white hover:bg-sky-500 md:h-9 md:w-9'>
 					{checkInCard(card, product) ? <MdShoppingCart /> : <MdAddShoppingCart />}
 				</button>
 
@@ -32,8 +39,10 @@ const ProductCardItem = ({ product }) => {
 					</Link>
 
 					<section className='flex items-center'>
-						<p className='mr-2 text-sm text-gray-400 line-through'>$ {price}</p>
-						<p className='text-sm'>$ {offPrice}</p>
+						<p className='bg-gray-20 mr-2 rounded-md bg-gray-200 px-1.5 text-sm text-gray-400 line-through'>
+							$ {price}
+						</p>
+						<p className='rounded-md bg-sky-200 px-1.5 text-sm'>$ {offPrice}</p>
 					</section>
 				</section>
 			</section>
--- src/components/Products/ProductListItem.jsx
+++ src/components/Products/ProductListItem.jsx
@@ -1,22 +1,27 @@
 import { useCardActions, useCard } from '../../Providers/CardProvider';
-import { MdAddShoppingCart,MdShoppingCart } from 'react-icons/md';
-import { Link } from 'react-router-dom'; 
+import { MdAddShoppingCart, MdShoppingCart } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
-import { checkInCard } from '../../utils/CheckInCard'; 
+import { checkInCard } from '../../utils/CheckInCard';
 
 const ProductListItem = ({ product }) => {
 	const { card } = useCard();
 	const dispatch = useCardActions();
-	const { name, price, image, offPrice } = product;
+	const { name, price, images, offPrice } = product;
 
 	const addProductHandler = (product) => {
 		dispatch({ type: 'ADD_TO_CARD', payload: product });
 	};
 
-		return (
-		<section className='m-1 flex w-11/12 items-center justify-between rounded-md border-1 px-2 py-1 hover:border-sky-500 hover:drop-shadow-xl sm:m-2 sm:w-10/12 md:w-7/12 lg:w-1/2'>
-			<Link to='/'>
-				<img src={image} alt={name} loading='lazy' className='h-20 w-20 rounded-md' />
+	return (
+		<section className='m-1 flex w-11/12 items-center justify-between rounded-md border-1 px-1.5 py-1.5 hover:border-sky-500 hover:drop-shadow-xl sm:m-2 sm:w-8/12 md:w-6/12 lg:max-w-sm xl:max-w-md'>
+			<Link to='/' className='h-20 w-20 hover:opacity-75 lg:h-24 lg:w-24'>
+				<img
+					src={images[0]}
+					alt={name}
+					loading='lazy'
+					className='h-auto w-full rounded-md'
+				/>
 			</Link>
 
 			<section className='my-1 flex flex-col items-start justify-center'>
@@ -26,15 +31,17 @@ const ProductListItem = ({ product }) => {
 					</Link>
 
 					<div className='flex items-center'>
-						<p className='mr-2 text-sm text-gray-400 line-through'>$ {price}</p>
-						<p className='text-sm'>$ {offPrice}</p>
+						<p className='mr-2 rounded-md bg-gray-200 px-1.5 text-sm text-gray-400 line-through'>
+							$ {price}
+						</p>
+						<p className='rounded-md bg-sky-200 px-1.5 text-sm'>$ {offPrice}</p>
 					</div>
 				</section>
 			</section>
 
 			<button
 				onClick={() => addProductHandler(product)}
-				className='flex h-7 w-7 items-center justify-center rounded-md bg-sky-400 text-lg text-white hover:bg-sky-500'>
+				className='flex h-7 w-7 items-center justify-center rounded-md bg-sky-400 text-lg text-white hover:bg-sky-500 sm:h-8 sm:w-8 lg:h-9 lg:w-9'>
 				{checkInCard(card, product) ? <MdShoppingCart /> : <MdAddShoppingCart />}
 			</button>
 		</section>
