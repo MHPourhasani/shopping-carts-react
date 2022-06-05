5fbff47c5bc69c85981b34dc9c8130a18e079a86
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-06-05 12:22:43 +0430

    header position fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { FiUser } from 'react-icons/fi';
 
 const Header = () => {
 	return (
-		<header className='flex h-11 w-full items-center justify-between bg-sky-500 px-5 text-white sm:h-14 lg:justify-around lg:text-lg'>
+		<header className='fixed top-0 right-0 z-10 flex h-12 w-full items-center justify-between bg-sky-500 px-5 text-white sm:h-14 lg:justify-around lg:text-lg'>
 			<Link to='/' className='text-lg font-bold'>
 				<p>MHP Shop</p>
 			</Link>
