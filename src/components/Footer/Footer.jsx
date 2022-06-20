2978cadd6c990f128a4d87e603089acf938581cd
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-06-20 18:07:23 +0430

    add active className from react-router-dom v6 and change footer height

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,42 +17,42 @@ const Footer = () => {
 	const { card } = useCard();
 
 	return (
-		<footer className='fixed bottom-0 right-0 flex h-11 w-full items-center justify-around rounded-t-lg bg-sky-500 font-semibold sm:h-14'>
+		<footer className='fixed bottom-0 right-0 flex h-14 w-full items-center justify-around rounded-t-lg bg-sky-500 font-semibold sm:h-14'>
 			<NavLink
 				to='/'
 				onClick={() => setActiveNav('home')}
-				className={`flex items-center justify-center ${
-					activeNav === 'home'
-						? 'rounded-md bg-sky-900 px-2 py-0.5 text-white sm:py-1.5 sm:px-3'
-						: 'opacity-60'
-				}`}>
+				className={({ isActive }) =>
+					isActive
+						? 'flex items-center justify-center rounded-md bg-sky-900 px-2 py-1.5 text-white sm:py-1.5 sm:px-3'
+						: 'flex items-center justify-center opacity-60'
+				}>
 				{activeNav === 'home' ? <AiFillHome /> : <AiOutlineHome />}
 				<p className='ml-1.5 text-sm'>Home</p>
 			</NavLink>
 
 			<NavLink
 				to='/cards'
-				onClick={() => setActiveNav('cardsPage')}
-				className={`relative flex items-center justify-center ${
-					activeNav === 'cardsPage'
-						? 'rounded-md bg-sky-900 px-2 py-0.5 text-white sm:py-1.5 sm:px-3'
-						: 'opacity-60'
-				}`}>
+				onClick={() => setActiveNav('cartsPage')}
+				className={({ isActive }) =>
+					isActive
+						? 'flex items-center justify-center rounded-md bg-sky-900 px-2 py-1.5 text-white sm:py-1.5 sm:px-3'
+						: 'flex items-center justify-center opacity-60'
+				}>
 				<p className='absolute -top-1 -right-4 flex h-4 w-4 items-center justify-center rounded-full bg-sky-700 text-xs '>
 					{card.length}
 				</p>
-				{activeNav === 'cardsPage' ? <AiTwotoneShopping /> : <AiOutlineShopping />}
-				<p className='ml-1.5 text-sm'>Cards</p>
+				{activeNav === 'cartsPage' ? <AiTwotoneShopping /> : <AiOutlineShopping />}
+				<p className='ml-1.5 text-sm'>Carts</p>
 			</NavLink>
 
 			<NavLink
 				to='/setting'
 				onClick={() => setActiveNav('setting')}
-				className={`flex items-center justify-center ${
-					activeNav === 'setting'
-						? 'rounded-md bg-sky-900 px-2 py-0.5 text-white sm:py-1.5 sm:px-3'
-						: 'opacity-60'
-				}`}>
+				className={({ isActive }) =>
+					isActive
+						? 'flex items-center justify-center rounded-md bg-sky-900 px-2 py-1.5 text-white sm:py-1.5 sm:px-3'
+						: 'flex items-center justify-center opacity-60'
+				}>
 				{activeNav === 'setting' ? <AiTwotoneSetting /> : <AiOutlineSetting />}
 				<p className='ml-1.5 text-sm'>Setting</p>
 			</NavLink>
