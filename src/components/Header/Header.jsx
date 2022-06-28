03d1f77169e66e09cee2a9cae8fe3e3c89e56120
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-06-28 18:45:29 +0430

    Check the user login and show the associated icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,21 @@
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../Providers/AuthProvider';
 
-import { FiUser } from 'react-icons/fi';
+import { FiUser, FiUserCheck } from 'react-icons/fi';
 
 const Header = () => {
+	const userData = useAuth();
+
 	return (
-		<header className='fixed top-0 right-0 z-10 flex h-12 w-full items-center justify-between bg-sky-500 px-5 text-white sm:h-14 lg:justify-around lg:text-lg'>
+		<header className='fixed top-0 right-0 z-10 flex h-14 w-full items-center justify-between bg-sky-500 px-5 text-white sm:h-14 lg:justify-around lg:text-lg'>
 			<Link to='/' className='text-lg font-bold'>
 				<p>MHP Shop</p>
 			</Link>
 
 			<Link
-				to='/login'
+				to={userData ? '/profile' : '/login'}
 				className='flex items-center justify-center rounded-full p-1 text-2xl hover:bg-gray-100 hover:text-black'>
-				<FiUser />
+				{userData ? <FiUserCheck /> : <FiUser />}
 			</Link>
 		</header>
 	);
