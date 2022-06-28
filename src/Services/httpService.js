219ab09d8c4e467c7b870bd539f12cd4846e36a1
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-06-28 16:28:42 +0430

    change baseURL to httpService

--- src/Services/httpService.js
+++ src/Services/httpService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://nodejs-post-app.herokuapp.com';
+// axios.defaults.baseURL = 'https://nodejs-post-app.herokuapp.com';
+axios.defaults.baseURL = 'http://localhost:5000/api';
 
 const http = {
 	get: axios.get,
