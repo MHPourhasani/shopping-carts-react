9e42a607b231896ae7c38aa3271bbeced3b1e82b
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-09-21 23:45:18 +0430

    change images path and add services

--- src/data.js
+++ src/data.js
@@ -1,53 +1,53 @@
-import alpha1 from './asset/image/alpha/alpha1.jpg';
-import alpha2 from './asset/image/alpha/alpha2.jpg';
-import alpha3 from './asset/image/alpha/alpha3.jpg';
+import alpha1 from './asset/images/alpha/alpha1.jpg';
+import alpha2 from './asset/images/alpha/alpha2.jpg';
+import alpha3 from './asset/images/alpha/alpha3.jpg';
 
-import jordan1 from './asset/image/jordan/jordan1.jpg';
-import jordan2 from './asset/image/jordan/jordan2.jpg';
-import jordan3 from './asset/image/jordan/jordan3.jpg';
+import jordan1 from './asset/images/jordan/jordan1.jpg';
+import jordan2 from './asset/images/jordan/jordan2.jpg';
+import jordan3 from './asset/images/jordan/jordan3.jpg';
 
-import skacher1 from './asset/image/skacher/skacher1.jpg';
-import skacher2 from './asset/image/skacher/skacher2.jpg';
-import skacher3 from './asset/image/skacher/skacher3.jpg';
-import skacher4 from './asset/image/skacher/skacher4.jpg';
-import skacher5 from './asset/image/skacher/skacher5.jpg';
+import skacher1 from './asset/images/skacher/skacher1.jpg';
+import skacher2 from './asset/images/skacher/skacher2.jpg';
+import skacher3 from './asset/images/skacher/skacher3.jpg';
+import skacher4 from './asset/images/skacher/skacher4.jpg';
+import skacher5 from './asset/images/skacher/skacher5.jpg';
 
-import reebok1 from './asset/image/reebok/reebok1.jpg';
-import reebok2 from './asset/image/reebok/reebok2.jpg';
-import reebok3 from './asset/image/reebok/reebok3.jpg';
+import reebok1 from './asset/images/reebok/reebok1.jpg';
+import reebok2 from './asset/images/reebok/reebok2.jpg';
+import reebok3 from './asset/images/reebok/reebok3.jpg';
 
-import adidas1 from './asset/image/adidas/adidas1.jpg';
-import adidas2 from './asset/image/adidas/adidas2.jpg';
-import adidas3 from './asset/image/adidas/adidas3.jpg';
+import adidas1 from './asset/images/adidas/adidas1.jpg';
+import adidas2 from './asset/images/adidas/adidas2.jpg';
+import adidas3 from './asset/images/adidas/adidas3.jpg';
 
-import ecco1 from './asset/image/ecco/ecco1.jpg';
-import ecco2 from './asset/image/ecco/ecco2.jpg';
-import ecco3 from './asset/image/ecco/ecco3.jpg';
+import ecco1 from './asset/images/ecco/ecco1.jpg';
+import ecco2 from './asset/images/ecco/ecco2.jpg';
+import ecco3 from './asset/images/ecco/ecco3.jpg';
 
-import calvin1 from './asset/image/calvin/calvin1.jpg';
-import calvin2 from './asset/image/calvin/calvin2.jpg';
+import calvin1 from './asset/images/calvin/calvin1.jpg';
+import calvin2 from './asset/images/calvin/calvin2.jpg';
 
-import lego1 from './asset/image/lego/lego1.jpg';
-import lego2 from './asset/image/lego/lego2.jpg';
-import lego3 from './asset/image/lego/lego3.jpg';
-import lego4 from './asset/image/lego/lego4.jpg';
-import lego5 from './asset/image/lego/lego5.jpg';
+import lego1 from './asset/images/lego/lego1.jpg';
+import lego2 from './asset/images/lego/lego2.jpg';
+import lego3 from './asset/images/lego/lego3.jpg';
+import lego4 from './asset/images/lego/lego4.jpg';
+import lego5 from './asset/images/lego/lego5.jpg';
 
-import yazi1 from './asset/image/yazi/yazi1.jpg';
-import yazi2 from './asset/image/yazi/yazi2.jpg';
-import yazi3 from './asset/image/yazi/yazi3.jpg';
+import yazi1 from './asset/images/yazi/yazi1.jpg';
+import yazi2 from './asset/images/yazi/yazi2.jpg';
+import yazi3 from './asset/images/yazi/yazi3.jpg';
 
-import cayana1 from './asset/image/cayana/cayana1.jpg';
-import cayana2 from './asset/image/cayana/cayana2.jpg';
-import cayana3 from './asset/image/cayana/cayana3.jpg';
+import cayana1 from './asset/images/cayana/cayana1.jpg';
+import cayana2 from './asset/images/cayana/cayana2.jpg';
+import cayana3 from './asset/images/cayana/cayana3.jpg';
 
-import scacherLogo from './asset/image/scacherLogo.png';
-import adidasLogo from './asset/image/adidasLogo.png';
-import eccoLogo from './asset/image/eccoLogo.png';
-import nikeLogo from './asset/image/nikeLogo.png';
-import reebokLogo from './asset/image/reebokLogo.png';
-import locasteLogo from './asset/image/locasteLogo.png';
-import asicsLogo from './asset/image/asicsLogo.png';
+import scacherLogo from './asset/images/scacherLogo.png';
+import adidasLogo from './asset/images/adidasLogo.png';
+import eccoLogo from './asset/images/eccoLogo.png';
+import nikeLogo from './asset/images/nikeLogo.png';
+import reebokLogo from './asset/images/reebokLogo.png';
+import locasteLogo from './asset/images/locasteLogo.png';
+import asicsLogo from './asset/images/asicsLogo.png';
 
 export const products = [
 	{
@@ -55,11 +55,7 @@ export const products = [
 		name: 'Alpha',
 		brand: 'Adidas',
 		brandLogo: adidasLogo,
-		descriptions: [
-			{ support: 'Lifetime warranty' },
-			{ support: 'Free Delivery' },
-			{ support: 'Original' },
-		],
+		services: ['Lifetime warranty', 'Free Delivery', 'Original'],
 		price: 120,
 		offPrice: 108,
 		discount: 12,
