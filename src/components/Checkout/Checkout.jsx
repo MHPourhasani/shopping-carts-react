b5bf3209f50220c007f4dafc9d5f9ceb34b07b0f
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-09-25 19:57:17 +0330

    improved some styles and write comments for different elements

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,10 +1,20 @@
+// Providers
 import { useAuth } from '../../Providers/AuthProvider';
 import { useCard, useCardActions } from '../../Providers/CardProvider';
 
-import { BsTrash } from 'react-icons/bs';
-import { toast } from 'react-toastify';
+// components
+import LoginPage from '../../Pages/LoginPage';
+import Layout from '../../Layout/Layout';
+
 import { Link } from 'react-router-dom';
-import empty_cart from '../../asset/image/empty_cart.png';
+import { toast } from 'react-toastify';
+
+// icons
+import { BiPlus, BiMinus } from 'react-icons/bi';
+import { BsTrash } from 'react-icons/bs';
+
+// images
+import empty_cart from '../../asset/images/empty-cart.png';
 
 const Checkout = () => {
 	const { card, total } = useCard();
@@ -27,141 +37,167 @@ const Checkout = () => {
 		dispatch({ type: 'DECREASE_FROM_CARD', payload: product });
 	};
 
+	// calcute products original total prices
 	const originalTotalPrice = card.length
 		? card.reduce((acc, curr) => acc + curr.quantity * curr.price, 0)
 		: 0;
 
 	if (!card.length) {
 		return (
-			<main className='flex min-h-screen w-full flex-col items-center justify-center'>
-				<img
-					src={empty_cart}
-					alt='empty cart'
-					loading='lazy'
-					className='w-11/12 md:w-9/12 lg:w-7/12 xl:mt-4'
-				/>
-				<Link
-					to='/'
-					className='mt-7 flex h-10 w-10/12 items-center justify-center rounded-md bg-sky-400 font-sans text-white lg:w-7/12'>
-					Go to Shopping
-				</Link>
-			</main>
+			<Layout>
+				<main className='flex min-h-screen w-full flex-col items-center justify-center overflow-hidden lg:mt-5'>
+					<img
+						src={empty_cart}
+						alt='empty cart image'
+						loading='lazy'
+						aria-hidden
+						className='w-11/12 md:w-9/12 lg:w-8/12 xl:w-5/12'
+					/>
+					<Link
+						to='/'
+						className='mt-10 flex w-11/12 items-center justify-center rounded-md bg-sky-400 py-2.5 font-medium text-white md:w-9/12 md:py-3 md:text-lg lg:w-8/12 xl:w-5/12'>
+						Go to Shopping
+					</Link>
+				</main>
+			</Layout>
 		);
 	}
 
 	return (
-		<main className='my-16 flex w-full flex-col items-center justify-center sm:my-20'>
+		<main className='flex w-full flex-col items-center justify-center pt-16 pb-24 lg:pt-20'>
 			{auth ? (
-				<section className='flex w-full flex-col items-center justify-center sm:flex-row sm:items-start'>
-					<section className='my-1.5 w-11/12 rounded-lg border-2 border-sky-500 bg-white p-3 shadow-sm sm:my-0 sm:mr-1 sm:w-6/12 md:w-7/12 lg:w-6/12 xl:max-w-lg'>
-						<p className='text-lg font-bold sm:text-base lg:text-lg'>Your Orders</p>
+				<section className='flex w-full flex-col items-center justify-center gap-3 sm:flex-row sm:items-start sm:gap-1'>
+					{/* User Orders section */}
+					<section className='w-11/12 rounded-lg border-2 border-sky-500 bg-white p-3 shadow-sm sm:mr-1 sm:w-6/12 md:w-7/12 lg:w-6/12 lg:p-5 xl:max-w-lg'>
+						<p className='text-lg font-bold sm:text-base md:text-lg lg:text-xl'>
+							Your Orders
+						</p>
 
 						{card &&
 							card.map((cart) => {
 								return (
-									<section
+									<div
 										key={cart.id}
-										className='bg-gray- my-2 flex w-full items-center justify-between rounded-md border-1 p-2 shadow-sm'>
+										className='flex w-full items-center justify-between border-b-1 py-4 shadow-sm hover:scale-105'>
+										{/* image of product and pass product data */}
 										<Link
 											to={`/products/${cart.name}`}
 											state={{ product: cart }}
-											className='hover:opacity-75'>
+											className='hover:opacity-90'>
 											<img
 												src={cart.images[0]}
 												alt={cart.name}
 												aria-hidden
-												className='h-14 w-14 rounded-md sm:h-16 sm:w-16 lg:h-20 lg:w-20'
+												className='h-20 w-20 rounded-md md:h-24 md:w-24 lg:h-28 lg:w-28 xl:h-32 xl:w-32'
 											/>
 										</Link>
 
-										<div>
-											<p className='mb-1 font-semibold'>{cart.name}</p>
-											<p className='text-sm'>$ {cart.offPrice}</p>
+										{/* product name and prices */}
+										<div className='flex flex-col justify-between'>
+											{/* name of product and pass product data */}
+											<Link
+												to={`/products/${cart.name}`}
+												state={{ product: cart }}
+												className='mb-1 font-semibold lg:text-lg'>
+												{cart.name}
+											</Link>
+
+											{/* show price and off price of product */}
+											<div className='my-2 flex gap-x-2 text-sm lg:gap-x-3 lg:text-base'>
+												<p className='rounded-md bg-sky-200 p-0.5 lg:px-1.5'>
+													$ {cart.offPrice}
+												</p>
+												<p className='text-gray-400 line-through'>
+													$ {cart.price}
+												</p>
+											</div>
 										</div>
 
 										<div className='flex items-center justify-center'>
 											{/* decrease product btn */}
 											<button
 												onClick={() => decProductHandler(cart)}
-												className='mr-1 flex h-6 w-6 items-center justify-center rounded-md bg-red-50 font-semibold text-red-600 shadow-md'>
-												{cart.quantity === 1 ? <BsTrash /> : '-'}
+												className='mr-1 flex h-6 w-6 items-center justify-center rounded-md bg-red-50 font-semibold text-red-600 shadow-sm hover:bg-red-100 lg:h-7 lg:w-7'>
+												{cart.quantity === 1 ? <BsTrash /> : <BiMinus />}
 											</button>
 
 											{/* products quantity btn */}
-											<span className='mx-1 flex h-6 w-6 items-center justify-center rounded-md bg-gray-100 shadow-md'>
+											<span className='mx-1 flex h-6 w-6 items-center justify-center rounded-md bg-gray-100 shadow-sm lg:h-7 lg:w-7'>
 												{cart.quantity}
 											</span>
 
 											{/* increase products btn */}
 											<button
 												onClick={() => addProductHandler(cart)}
-												className='ml-1 flex h-6 w-6 items-center justify-center rounded-md bg-green-100 font-semibold text-green-600 shadow-md'>
-												+
+												className='ml-1 flex h-6 w-6 items-center justify-center rounded-md bg-green-100 font-semibold text-green-600 shadow-sm hover:bg-green-200 lg:h-7 lg:w-7'>
+												<BiPlus />
 											</button>
 										</div>
-									</section>
+									</div>
 								);
 							})}
 					</section>
 
-					<section className='my-1.5 w-11/12 rounded-lg border-2 border-sky-500 bg-white p-3 shadow-sm sm:my-0 sm:ml-1 sm:w-5/12 md:w-4/12 lg:w-64 xl:max-w-sm'>
-						<div className='border-b-1 border-gray-400'>
-							<p className='mb-2 text-lg font-bold sm:text-base lg:text-lg'>
+					{/* User Information and Cart Summary sections */}
+					<section className='flex w-11/12 flex-col gap-y-2 rounded-lg border-2 border-sky-500 bg-white p-3 shadow-sm sm:ml-1 sm:w-5/12 md:w-4/12 xl:max-w-sm'>
+						{/* User Information section */}
+						<div className='flex flex-col gap-2.5 border-b-1 border-gray-400 pb-5'>
+							<p className='text-lg font-bold sm:text-base md:text-lg lg:text-xl'>
 								Your Information
 							</p>
 
-							<div className='my-1.5 flex items-center justify-start text-sm lg:text-base'>
+							{/* name of user */}
+							<div className='flex items-center gap-x-2 text-sm lg:text-base'>
 								<p className='font-semibold'>Name:</p>
-								<p className='ml-2 font-medium'>{auth.name}</p>
+								<p className='font-medium'>{auth.name}</p>
 							</div>
 
-							<div className='my-1.5 flex items-center justify-start text-sm lg:text-base'>
+							{/* phone number of user */}
+							<div className='flex items-center gap-x-2 text-sm lg:text-base'>
 								<p className='font-semibold'>Phone Number:</p>
-								<p className='ml-2 font-medium'>{auth.phoneNumber}</p>
+								<p className='font-medium'>{auth.phoneNumber}</p>
 							</div>
 
-							<div className='my-1.5 flex items-center justify-start text-sm lg:text-base'>
+							{/* email of user */}
+							<div className='flex items-center gap-x-2 text-sm lg:text-base'>
 								<p className='font-semibold'>Email:</p>
-								<p className='ml-2 font-medium'>{auth.email}</p>
+								<p className='font-medium'>{auth.email}</p>
 							</div>
 						</div>
 
+						{/* Cart Summary section */}
 						<div className='my-1.5'>
-							<p className='mb-2 text-lg font-bold sm:text-base lg:text-lg'>
+							<p className='mb-2 text-lg font-bold sm:text-base md:text-lg lg:text-xl'>
 								Cart Summary
 							</p>
 
-							<section>
-								<div className='my-2 flex items-center justify-between text-sm'>
-									<p className='font-semibold'>Total Price</p>
-									<p className='font-medium'>$ {originalTotalPrice}</p>
-								</div>
+							{/* show original total price and off price and total Price */}
+							<section className='flex flex-col gap-y-2.5'>
+								<span className='flex items-center justify-between text-sm font-medium lg:text-base'>
+									<p>Original Total Price</p>
+									<p>$ {originalTotalPrice.toFixed(2)}</p>
+								</span>
 
-								<div className='my-2 flex items-center justify-between text-sm'>
-									<p className='font-semibold'>Off Price</p>
-									<p className='font-medium'>$ {originalTotalPrice - total}</p>
-								</div>
+								<span className='flex items-center justify-between text-sm font-medium lg:text-base'>
+									<p>Off Price</p>
+									<p>$ {(originalTotalPrice - total).toFixed(2)}</p>
+								</span>
 
-								<div className='my-2 flex items-center justify-between text-sm'>
-									<p className='font-semibold'>Net Price</p>
-									<p className='font-medium'>$ {total}</p>
-								</div>
+								<span className='flex items-center justify-between text-sm font-semibold lg:text-base'>
+									<p>Total Price</p>
+									<p>$ {total.toFixed(2)}</p>
+								</span>
 							</section>
 
-							<button className='mt-2 w-full rounded-md bg-sky-400 py-1.5 text-lg font-semibold text-white shadow-sm'>
-								Pay
+							{/* Payment btn */}
+							<button className='mt-5 w-full rounded-md bg-sky-500 py-2 text-lg font-semibold text-white shadow-sm hover:bg-sky-600'>
+								Payment
 							</button>
 						</div>
 					</section>
 				</section>
 			) : (
-				<section>
-					<Link
-						to='/login'
-						className='flex h-10 w-10/12 items-center justify-center rounded-md'>
-						Please login!
-					</Link>
-				</section>
+				<LoginPage />
 			)}
 		</main>
 	);
