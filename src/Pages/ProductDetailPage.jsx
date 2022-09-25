1370d87973a536517f851dac365691a8745a1b2f
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2022-09-26 02:03:16 +0330

    improving some styles and improving the customer experience

--- src/Pages/ProductDetailPage.jsx
+++ src/Pages/ProductDetailPage.jsx
@@ -14,7 +14,8 @@ import 'swiper/css/navigation';
 
 // icons
 import { MdAddShoppingCart } from 'react-icons/md';
-import { AiTwotoneShopping } from 'react-icons/ai';
+import { BsClipboardCheck } from 'react-icons/bs';
+import { HiOutlineShoppingCart } from 'react-icons/hi';
 
 const ProductDetailPage = () => {
 	const { state } = useLocation();
@@ -31,9 +32,9 @@ const ProductDetailPage = () => {
 
 	return (
 		<Layout>
-			<section className='flex w-full flex-col items-center justify-center gap-3 py-16 sm:pt-16 md:flex-row md:items-start md:gap-5 md:pt-20'>
-				{/* image slider */}
-				<div className='flex w-11/12 items-center justify-center md:w-5/12 xl:max-w-lg'>
+			<section className='flex w-full flex-col items-center justify-center pt-16 pb-24 sm:pt-16 md:pt-20 lg:flex-row lg:items-start lg:gap-5'>
+				{/* image slider with Swiper */}
+				<div className='flex w-11/12 items-center justify-center md:w-10/12 lg:w-5/12 xl:max-w-lg'>
 					<Swiper
 						slidesPerView={1}
 						spaceBetween={30}
@@ -43,7 +44,7 @@ const ProductDetailPage = () => {
 						}}
 						breakpoints={{
 							640: { slidesPerView: 2, spaceBetween: 10 },
-							768: { slidesPerView: 1, spaceBetween: 0 },
+							1024: { slidesPerView: 1, spaceBetween: 0 },
 						}}
 						navigation={true}
 						modules={[Pagination, Navigation]}>
@@ -62,40 +63,47 @@ const ProductDetailPage = () => {
 					</Swiper>
 				</div>
 
-				<div className='flex w-11/12 flex-col items-start justify-between md:w-6/12 xl:max-w-xl'>
-					{/* product name and price */}
-					<section className='my-2 flex w-full items-center justify-between'>
+				{/* product information section */}
+				<div className='flex w-11/12 flex-col items-start justify-between md:w-10/12 lg:w-6/12 xl:max-w-xl'>
+					{/* product name and prices */}
+					<section className='my-2 flex w-full items-center justify-between sm:my-2'>
+						{/* show name and brand of product */}
 						<div>
-							<p className='mb-2 text-2xl font-bold lg:text-4xl'>{product.name}</p>
-							<div className='flex h-5 w-auto items-center justify-start rounded-md bg-sky-100 p-1'>
-								<p className='mr-2 text-xs font-semibold lg:text-base'>Brand:</p>
-								<div className='flex h-5 w-auto items-center justify-center text-sm lg:text-base'>
-									<img
-										src={product.brandLogo}
-										alt={product.name}
-										className='mr-1 h-full'
-									/>
-								</div>
+							{/* product name */}
+							<p className='mb-2 text-2xl font-bold md:mb-3 md:text-3xl lg:text-4xl'>
+								{product.name}
+							</p>
+							{/* product brand logo */}
+							<div className='flex w-auto items-center justify-start gap-x-2.5 rounded-md bg-sky-100 p-1'>
+								<p className='text-xs font-semibold md:text-sm lg:text-base'>
+									Brand:
+								</p>
+								<img
+									src={product.brandLogo}
+									alt={product.name}
+									className='h-5 w-auto md:h-6'
+								/>
 							</div>
 						</div>
 
-						<div>
-							<p className='mb-1 rounded-md bg-sky-200 py-1 px-2 text-sm font-semibold sm:text-base lg:text-lg'>
+						{/* show price and off price of product */}
+						<div className='text-sm md:text-base xl:text-lg'>
+							<p className='mb-1.5 rounded-md bg-sky-200 py-1 px-2 font-semibold'>
 								$ {product.offPrice}
 							</p>
-							<p className='rounded-md bg-gray-200 px-2 text-sm text-gray-500 line-through lg:text-lg'>
+							<p className='rounded-md bg-gray-200 px-2 py-1 text-gray-500 line-through'>
 								$ {product.price}
 							</p>
 						</div>
 					</section>
 
-					{/* product size */}
-					<section className='my-1 flex w-full flex-wrap items-center justify-start gap-1.5'>
+					{/* product sizes */}
+					<section className='my-2 flex w-full flex-wrap items-center justify-start gap-1.5'>
 						{product.sizes.map((size) => {
 							return (
 								<span
 									key={size}
-									className='flex h-9 w-9 cursor-pointer items-center justify-center rounded-md bg-gray-200 shadow-sm hover:bg-gray-300 lg:h-11 lg:w-11'>
+									className='flex h-9 w-9 cursor-pointer items-center justify-center rounded-md bg-gray-200 shadow-sm hover:bg-gray-300 md:h-10 md:w-10 lg:h-11 lg:w-11'>
 									{size}
 								</span>
 							);
@@ -110,37 +118,48 @@ const ProductDetailPage = () => {
 						nostrum deleniti.
 					</div>
 
-					<div className='my-1 flex flex-wrap items-center justify-start pb-12 text-sm sm:justify-center sm:pb-14 md:pb-2'>
+					{/* product services */}
+					<div className='my-1 flex flex-wrap items-center justify-start gap-2 pb-12 text-sm sm:pb-14 md:pb-2'>
 						{product.services.map((service) => {
 							return (
-								<p
-									key={service}
-									className='md: m-1 rounded-md bg-gray-200 px-2 py-0.5'>
+								<p key={service} className='rounded-md bg-gray-200 px-2 py-1'>
 									{service}
 								</p>
 							);
 						})}
 					</div>
 
-					{/* add product btn */}
-					<button
-						type='submit'
-						onClick={() => addProductHandler(product)}
-						className='fixed bottom-14 my-2 h-9 w-11/12 rounded-md bg-sky-400 text-white hover:bg-sky-500 sm:bottom-14 md:static md:h-10 md:w-full lg:h-11'>
+					{/* checking the product in the shopping cart */}
+					<section className='fixed bottom-20 my-2.5 w-11/12 md:static md:w-full'>
 						{checkInCard(card, product) ? (
-							<Link
-								to='/cards'
-								className='flex items-center justify-center font-semibold'>
-								<AiTwotoneShopping className='mr-2 text-xl' />
-								<p>Go to your Cart</p>
-							</Link>
-						) : (
-							<section className='flex items-center justify-center font-semibold'>
-								<MdAddShoppingCart className='mr-2 text-xl' />
-								<p> Add to Card</p>
+							<section className='flex w-full items-center justify-between gap-2'>
+								{/* continue shopping btn */}
+								<Link
+									to='/'
+									className='flex h-10 flex-auto cursor-pointer items-center justify-center rounded-md bg-gray-200 font-semibold text-gray-900 hover:bg-gray-300 sm:h-11 lg:h-12'>
+									<HiOutlineShoppingCart className='mr-2 text-xl' />
+									<p>Continue shopping</p>
+								</Link>
+
+								{/* go to checkout btn */}
+								<Link
+									to='/checkout'
+									className='flex h-10 flex-auto cursor-pointer items-center justify-center rounded-md bg-sky-500 font-semibold text-white hover:bg-sky-600 sm:h-11 lg:h-12'>
+									<BsClipboardCheck className='mr-2 text-xl' />
+									<p>Go to Checkout</p>
+								</Link>
 							</section>
+						) : (
+							//  add product to cart btn
+							<button
+								type='submit'
+								onClick={() => addProductHandler(product)}
+								className='flex h-10 w-full items-center justify-center gap-2 rounded-md bg-sky-500 font-semibold text-white hover:bg-sky-600 sm:h-11 lg:h-12'>
+								<MdAddShoppingCart className='text-xl' />
+								<p>Add to Cart</p>
+							</button>
 						)}
-					</button>
+					</section>
 				</div>
 			</section>
 		</Layout>
