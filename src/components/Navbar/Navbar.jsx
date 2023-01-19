845770fb5d1d3f2af42d972733c5d42580c37bb1
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-01-20 01:14:21 +0330

    Redesign Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,68 +2,47 @@ import { NavLink } from 'react-router-dom';
 import { useCard } from '../../Providers/CardProvider';
 
 // icons
+import { BsBagDash, BsCartDash } from 'react-icons/bs';
+import { IoBagRemoveOutline } from 'react-icons/io';
+import { RiHomeSmile2Line } from 'react-icons/ri';
 import { AiOutlineSetting } from 'react-icons/ai';
-import { RiHomeLine, RiHomeSmile2Line } from 'react-icons/ri';
-import { BsBagDash } from 'react-icons/bs';
 
 const Navbar = () => {
 	const { card } = useCard();
 
 	return (
-		<nav className='fixed bottom-0 flex h-16 w-11/12 -translate-y-4 items-center justify-between rounded-md border-2 border-sky-500 bg-white px-1.5 font-semibold shadow-md md:hidden'>
+		<nav className='fixed bottom-0 flex w-full items-center justify-between rounded-t-2xl border-t-1 border-gray-300 bg-white px-5 pb-1.5 pt-3 font-medium shadow-navBar md:hidden'>
 			{/* Carts icon */}
 			<NavLink
 				to='/carts'
-				className={({ isActive }) =>
-					isActive
-						? 'relative flex h-12 items-center justify-center gap-x-1 rounded-md bg-sky-500 px-3 text-white sm:py-1.5 sm:px-3'
-						: 'relative flex items-center justify-center px-2 py-1.5 text-gray-600'
-				}>
-				{({ isActive }) => (
-					<>
-						<BsBagDash className='h-auto w-7' />
-						{isActive ? <p className='text-sm'>Carts</p> : null}
-						<span className='absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-sky-700 text-xs'>
-							{card.length}
-						</span>
-					</>
-				)}
+				className={({ isActive }) => (isActive ? 'text-sky-500' : 'text-gray-400')}>
+				<span className='relative flex flex-col items-center justify-center gap-0.5'>
+					<BsCartDash className='h-auto w-7' />
+					<p className='text-sm'>Carts</p>
+					<span className='absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-gray-300 text-xs'>
+						{card.length}
+					</span>
+				</span>
 			</NavLink>
 
 			{/* Home icon */}
 			<NavLink
 				to='/'
-				className={({ isActive }) =>
-					isActive
-						? 'flex h-12 items-center justify-center gap-x-1 rounded-md bg-sky-500 px-3 text-white sm:py-1.5 sm:px-3'
-						: 'flex items-center justify-center text-gray-600'
-				}>
-				{({ isActive }) => (
-					<>
-						{isActive ? (
-							<RiHomeSmile2Line className='h-auto w-8' />
-						) : (
-							<RiHomeLine className='h-auto w-8' />
-						)}
-						{isActive ? <p className='text-sm'>Home</p> : null}
-					</>
-				)}
+				className={({ isActive }) => (isActive ? 'text-sky-500' : ' text-gray-400')}>
+				<span className='flex flex-col items-center justify-center gap-0.5'>
+					<RiHomeSmile2Line className='h-auto w-7' />
+					<p className='text-sm'>Home</p>
+				</span>
 			</NavLink>
 
 			{/* Setting icon */}
 			<NavLink
 				to='/setting'
-				className={({ isActive }) =>
-					isActive
-						? 'flex h-12 items-center justify-center gap-x-1 rounded-md bg-sky-500 px-3 text-white sm:py-1.5 sm:px-3'
-						: 'flex items-center justify-center text-gray-600'
-				}>
-				{({ isActive }) => (
-					<>
-						<AiOutlineSetting className='h-auto w-7' />
-						{isActive ? <p className='text-sm'>Setting</p> : null}
-					</>
-				)}
+				className={({ isActive }) => (isActive ? 'text-sky-500' : 'text-gray-400')}>
+				<span className='flex flex-col items-center justify-center gap-0.5'>
+					<AiOutlineSetting className='h-auto w-7' />
+					<p className='text-sm'>Setting</p>
+				</span>
 			</NavLink>
 		</nav>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
 			borderWidth: {
 				1: '1px',
 			},
+			boxShadow: {
+				navBar: '0px 70px 60px 60px rgba(0,0,0,0.75)',
+			},
 		},
 	},
 	plugins: [],
