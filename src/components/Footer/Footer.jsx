6cfa4fff3873ce02eebcb4116f16e14df0dc45fe
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-05-26 18:22:34 +0330

    changed the design of the Footet component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 // icons
 import { AiOutlineCopyrightCircle } from 'react-icons/ai';
 import { BsArrowUpSquare } from 'react-icons/bs';
+import UpIcon from '../../asset/svg/Up';
 
 const Footer = () => {
 	// goToTop function to go to the top of the page
@@ -9,24 +10,24 @@ const Footer = () => {
 	};
 
 	return (
-		<footer className='mt-5 flex h-48 w-full flex-col items-center justify-start gap-4 bg-sky-500 px-2 py-3 sm:h-36 sm:flex-row sm:items-start sm:justify-between sm:p-5 md:h-16 md:items-center xl:h-14'>
+		<footer className='mt-5 flex h-48 w-full flex-col items-center justify-start gap-4 border-1 bg-white px-2 py-3 sm:h-36 sm:flex-row sm:items-start sm:justify-between sm:p-5 md:h-auto md:items-center'>
 			{/* copyright section */}
-			<section className='flex flex-col items-center gap-1 sm:flex-row'>
-				<AiOutlineCopyrightCircle className='hidden h-auto w-5 text-gray-200 sm:block' />
-				<p className='text-sm text-gray-200'>Designed and developed by</p>
+			<span className='flex flex-col items-center gap-1 sm:flex-row'>
+				<AiOutlineCopyrightCircle className='hidden h-auto w-5 text-gray-400 sm:block' />
+				<p className='text-sm text-gray-400'>Designed and developed by</p>
 				<a
 					href='https://mh-pourhasani.vercel.app/'
 					target='_blank'
 					rel='noopener noreferrer'
-					className='font-semibold text-white hover:text-sky-900'>
+					className='font-semibold text-gray-500 hover:text-gray-600'>
 					Mohammad Hasan Pourhasani
 				</a>
-			</section>
+			</span>
 
 			{/* go to top btn */}
-			<BsArrowUpSquare
+			<UpIcon
 				onClick={() => goToTop()}
-				className='h-auto w-7 cursor-pointer text-gray-300 hover:text-gray-50 sm:w-9'
+				className='h-auto w-7 cursor-pointer stroke-slate-400 transition-all ease-in-out hover:stroke-sky-500 sm:w-7'
 			/>
 		</footer>
 	);
