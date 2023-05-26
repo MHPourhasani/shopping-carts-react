6bda824fa08db691cdb84be4518384b67cce68a6
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-05-26 17:56:22 +0330

    added custom boxShadow to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
 				1: '1px',
 			},
 			boxShadow: {
-				navBar: '0px 70px 60px 60px rgba(0,0,0,0.75)',
+				header: '0px -50px 60px 25px rgba(0,0,0,0.75)',
+				navBar: '0px 70px 60px 60px rgba(0,0,0,0.75)'
 			},
 		},
 	},
