8a43e68bef1a24c62e15889012ac371ec51a2e40
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-05-26 18:23:55 +0330

    updated Home page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,12 +2,16 @@
 import Layout from '../Layout/Layout';
 import ProductList from '../components/Products/ProductList';
 import Footer from '../components/Footer/Footer';
+import AsideMenu from '../components/AsideMenu/AsideMenu';
 
 const HomePage = () => {
 	return (
 		<Layout>
-			<main className='w-full'>
-				<ProductList />
+			<main className='flex w-full flex-col bg-white pt-16 pb-8 md:pt-20 md:pb-0'>
+				<section className='flex items-start justify-between lg:px-10'>
+					<AsideMenu/>
+					<ProductList />
+				</section>
 				<Footer />
 			</main>
 		</Layout>
